3a51816e19d78a2b0a4f2bd2c26d0d51fe945867
Nicole Santos <gitnicole.santos@gmail.com>
2026-06-17 10:56:18 -0300

    Criacao de l041q11.js e l041q12.js e alteracoes em l041q06.js e l041q07.js

---
+++ l041q11.js
@@ -0,0 +1,9 @@
+// 11) Desenvolver um programa que pergunte um número de 3 casas e apresente como resultado somente o
+// algarismo das centenas.
+
+let num;
+
+console.log("Informe um número a partir das centenas: ")
+num = 555
+console.log(`Número: ${num}`)
+
---
+++ l041q12.js
@@ -0,0 +1,27 @@
+//12) Desenvolver um programa que pergunte 5 números inteiros e identifique o maior número e o menor número.
+
+let num1, num2, num3, num4, num5;
+
+console.log("Informe o primeiro número: ")
+num1 = 5
+console.log(`${num1}`)
+
+console.log("Informe o segundo número: ")
+num2 = 8
+console.log(`${num2}`)
+
+console.log("Informe o terceiro número: ")
+num3 = 9
+console.log(`${num3}`)
+
+console.log("Informe o quarto número: ")
+num4 = 15
+console.log(`${num4}`)
+
+console.log("Informe o quinto número: ")
+num5 = 28
+console.log(`${num5}`)
+
+if (num ){
+
+}
