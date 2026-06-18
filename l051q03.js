a8b80c3538897b30c1ed588e7df9da6f9d449e59
Nicole Santos <gitnicole.santos@gmail.com>
2026-06-18 10:15:17 -0300

    Exemplos laços de repeticao While

---
+++ l051q03.js
@@ -0,0 +1,11 @@
+/*3) Desenvolver um programa que apresente os quadrados dos números inteiros de 15 a 200.
+*/
+
+let num;
+num = 15;
+
+while (num <= 200){
+    
+    console.log(`${num} elevado ao quadrado é igual = ${num ** 2}`)
+    num = num + 1; 
+}
---
+++ l051q05.js
@@ -0,0 +1,14 @@
+/*Desenvolver um programa que apresente os resultados de uma tabela de um número qualquer. Ela deve ser
+impressa no seguinte formato: */
+
+let cont = 1;
+let num = 2;
+multi = 0; 
+res = 0;
+
+while( cont <= 10){
+    multi += 1
+    res = num * multi 
+    console.log(`${num} . ${multi} = ${res}`)
+    cont += 1
+}
