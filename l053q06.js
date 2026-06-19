71dce6c48597f7927899439eded4f16d64ae47e0
Nicole Santos <gitnicole.santos@gmail.com>
2026-06-19 11:47:10 -0300

    Alteracoes em l053q06.js

--- l053q06.js
+++ l053q06.js
@@ -4,12 +4,14 @@ qualquer, ou seja, de be , onde os valores de b e e são fornecidos pelo usuári
 let cont;
 let base;
 let expo;
-let pot;
+let pot = 1;
 
-
+console.log("Infome o valor da base da potência: ")
 base = 2
+console.log(`Base: ${base}`)
+console.log("Informe o valor do expoente da potência: ")
 expo = 6
-pot = 1
+console.log(`Expoente: ${expo}`)
 
 for(cont = 1; cont <= expo; cont++){
     pot = pot * base
